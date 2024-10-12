027ffcbd107b57858060cae0265bcc91c43e0013
Dayane <dayane.rodrigues@bcrcx.com>
2024-10-12 17:44:57 -0300

    App buscador de receitas

---
+++ scripts.js
@@ -0,0 +1,67 @@
+const form = document.querySelector('.search-form');
+const recipeList = document.querySelector('.recipe-list');
+const recipeDetails = document.querySelector('.recipe-details')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const inputValue = (e.target[0].value)
+
+    searchRecipes(inputValue)
+})
+
+async function searchRecipes(ingredient) {
+     recipeList.innerHTML= `<p>Loading...</p>`
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+        const data = await response.json()
+
+        showRecipes(data.meals)
+    } catch(err){
+        recipeList.innerHTML= `<p>Recipe not found</p>`
+    }
+}
+
+function showRecipes(recipes) {
+    recipeList.innerHTML = recipes.map(item => `
+        
+        <div class="recipe-card" onclick="getRecipesDetails(${item.idMeal})">
+            <img src="${item.strMealThumb}" alt"${item.strMeal}">
+            <h3>${item.strMeal}</h3>
+        </div>
+        `
+    ).join('')
+}
+
+async function getRecipesDetails(id) {
+    try{
+
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    const data = await response.json()
+    const recipe = data.meals[0]
+
+    let ingredients = ''
+
+    for (let i = 1; i <= 20; i++) {
+        if (recipe[`strIngredient${i}`]) {
+            ingredients += `<li>${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]}</li>`
+        } else {
+            break;
+        }
+    }
+
+
+    recipeDetails.innerHTML = `
+    <h2>${recipe.strMeal}</h2>
+    <img src="${recipe.strMealThumb}" alt"${recipe.strMeal}">
+    <h3>Ingredients:</h3>
+    <ul>${ingredients}</ul>
+    <h3>Instructions</h3>
+    <p>${recipe.strInstructions}</p>
+
+    <p>Tags: ${recipe.strTags}</p>
+    <p>Vídeo: <a href="${recipe.strYoutube}" target="_blank">Play on Youtube</a></p>
+    `
+    }catch(err){
+         recipeDetails.innerHTML= `<p>Recipe not found</p>`
+    }
+}
